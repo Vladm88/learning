bebb52a2ba1ee1a9e79b4b292abbe553f1df7307
Влад <vladmikolaenko@outlook.com>
2016-04-07 20:21:23 +0300

    Можно делать ход

--- krestiki-noliki.pas
+++ krestiki-noliki.pas
@@ -2,7 +2,7 @@ Program qwe;
 Uses crt;
 var 
   Field: array [1..3] of array [1..3] of integer;
-  i,j:integer;
+  i,j,Position:integer;
   
 Procedure ShowField;  
 var
@@ -31,15 +31,11 @@ End;
   
 Begin
   ClearField();  
-  Field[1,1]:=2;
-  Field[2,2]:=1;
-  Field[3,3]:=2;
-  ShowField();    
-   writeln();
-  ClearField(); 
-  Field[1,1]:=2;
-  Field[1,2]:=1;
-  Field[1,3]:=2;
+  while true do begin
   ShowField();
-  
+  write('Сделайте ход: ');
+  read(Position);
+  Field[(Position-1)div 3+1,(Position-1) mod 3+1]:=2;
+  writeln();
+  end;
 End.
